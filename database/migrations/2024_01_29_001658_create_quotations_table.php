f27d925032631492070c443c1c54554403d2f236
Abrarulhassan-786 <abrarulhassan336@gmail.com>
2024-02-01 02:01:08 +0500

    quotation table issue fix

--- database/migrations/2024_01_29_001658_create_quotations_table.php
+++ database/migrations/2024_01_29_001658_create_quotations_table.php
@@ -13,6 +13,7 @@ class CreateQuotationsTable extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('quotations');
         Schema::create('quotations', function (Blueprint $table) {
             $table->id();
             $table->string("remarks");
@@ -23,8 +24,8 @@ class CreateQuotationsTable extends Migration
             $table->unsignedBigInteger('depart_id');
             $table->foreign('depart_id')->references('id')->on('departments')->onDelete('cascade');
             // supplier foreign key
-            $table->unsignedBigInteger('supplier_id');
-            $table->foreign('supplier_id')->references('supplier_id')->on('suppliers')->onDelete('cascade')->collation('utf8_unicode_ci');
+            $table->unsignedInteger('supplier_id');
+            $table->foreign('supplier_id')->references('suplier_id')->on('suppliers')->onDelete('cascade');
             $table->timestamps();
         });
     }
